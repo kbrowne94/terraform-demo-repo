8004b3f6e8e409b2290c668fbe66df0875381381
Kyle Browne <kbrowne5@my.bcit.ca>
2020-04-27 12:04:08 -0700

    demo

---
+++ kyle-demo.tf
@@ -0,0 +1,45 @@
+provider "aws" {}
+
+resource "aws_security_group" "demo_allow_http_ssh" {
+  name        = "demo_allow_http_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami             = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.demo_allow_http_ssh.name}"]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
