5debb7aefa424bc77f9b58055a719fca0fba1864
Kyle Browne <kbrowne5@my.bcit.ca>
2020-04-29 11:10:11 -0700

    pipeline test

--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@
 # use . ./env.sh
 export AWS_ACCESS_KEY_ID="<ENTER KEY>"
 export AWS_SECRET_ACCESS_KEY="<ENTER SECRET>"
-export AWS_DEFAULT_REGION="us-west-2"
+export AWS_DEFAULT_REGION="<us-west-2>"
